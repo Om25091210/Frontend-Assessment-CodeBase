823cf033090d5743be6ca1591d053cf0b5b6ff52
omyadav-wyle <om.yadav@wyleglobal.com>
2026-01-17 15:16:00 +0530

    Added the Cloud run.

--- src/app/booking.service.ts
+++ src/app/booking.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../environments/environment';
 export interface Room {
   number: number;
   floor: number;
@@ -15,7 +15,7 @@ export interface Room {
 })
 export class BookingService {
   // Ensure this matches your Node backend URL
-  private apiUrl = 'http://localhost:3000'; 
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
